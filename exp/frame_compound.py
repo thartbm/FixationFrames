b0002ad6c01f2a68bb27ec7a636dc363afe91cf5
Marius 't Hart <thartbm@gmail.com>
2026-01-27 10:07:56 -0500

    extremer amplitude spread

--- exp/frame_compound.py
+++ exp/frame_compound.py
@@ -1083,10 +1083,10 @@ def getTasks(cfg):
                 #  {'test':'anglespread', 'period':1/4, 'amplitude':4, 'stimtype':'compoundframe', 'timespread':0, 'anglespread':40, 'amplitudespread':0, 'framesize':[7,7], 'maxdotlife':1/2},
 
                  {'test':'combined', 'period':1/4, 'amplitude':4, 'stimtype':'compoundframe', 'timespread':0.200, 'anglespread':90, 'amplitudespread':4.0, 'framesize':[7,7], 'maxdotlife':1/8},
-                 {'test':'combined', 'period':1/4, 'amplitude':4, 'stimtype':'compoundframe', 'timespread':0.200, 'anglespread':90, 'amplitudespread':6.0, 'framesize':[7,7], 'maxdotlife':1/8},
                  {'test':'combined', 'period':1/4, 'amplitude':4, 'stimtype':'compoundframe', 'timespread':0.200, 'anglespread':90, 'amplitudespread':8.0, 'framesize':[7,7], 'maxdotlife':1/8},
-                 {'test':'combined', 'period':1/4, 'amplitude':4, 'stimtype':'compoundframe', 'timespread':0.200, 'anglespread':90, 'amplitudespread':10.0, 'framesize':[7,7], 'maxdotlife':1/8},
                  {'test':'combined', 'period':1/4, 'amplitude':4, 'stimtype':'compoundframe', 'timespread':0.200, 'anglespread':90, 'amplitudespread':12.0, 'framesize':[7,7], 'maxdotlife':1/8},
+                 {'test':'combined', 'period':1/4, 'amplitude':4, 'stimtype':'compoundframe', 'timespread':0.200, 'anglespread':90, 'amplitudespread':16.0, 'framesize':[7,7], 'maxdotlife':1/8},
+                 {'test':'combined', 'period':1/4, 'amplitude':4, 'stimtype':'compoundframe', 'timespread':0.200, 'anglespread':90, 'amplitudespread':20.0, 'framesize':[7,7], 'maxdotlife':1/8},
 
 
                 #  {'test':'classic', 'period':1/4, 'amplitude':4.0, 'stimtype':'classicframe'},
